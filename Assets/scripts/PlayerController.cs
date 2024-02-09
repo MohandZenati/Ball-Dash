8ae0cc4cf7c4b97025682ce5af5516e44668e708
MohandZenati <81512623+MohandZenati@users.noreply.github.com>
2024-02-09 10:16:48 +0100

    add gravity, movement and collision

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float gravity = -9.81f;
+    Vector3 velocity;
+    public LayerMask groundMask;
+    public float groundDistance = 0.4f;
+    bool isGrounded;
+    private float horizontalInput;
+
+    public float moveSpeed = 2f;
+    CharacterController characterController;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        
+    }
+
+    void Update()
+    {
+        // Vérifier si le joueur est au sol
+        horizontalInput = 1;
+        isGrounded = Physics.CheckSphere(transform.position, groundDistance, groundMask);
+
+        transform.forward = new Vector3(horizontalInput, 0, Mathf.Abs(horizontalInput) - 1);
+
+        // Si le joueur est au sol et la vitesse verticale est négative, réinitialiser la vitesse verticale
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        characterController.Move(new Vector3(horizontalInput, 0, 0) * moveSpeed * Time.deltaTime);
+
+        // Calculer la gravité
+        velocity.y += gravity * Time.deltaTime;
+
+        // Appliquer la gravité
+        characterController.Move(velocity * Time.deltaTime);
+    }
+}
